8907e7b272b621bf59e1888969c66c7c7ccdfb7e
shahpuj <pujan@vt.edu>
2026-02-13 10:59:45 -0500

    began work on CAN decoding
    fixed some formatting with clangd
    added callbacks and callback injection for both CAN busses. Need to continue implementing decoder logic with hash/function pointer table and vehicle struct injection for decode data destination

--- include/can.h
+++ include/can.h
@@ -1,35 +1,48 @@
 #pragma once
+#include <cstdint>
 #include <zephyr/drivers/can.h>
 #include <zephyr/kernel.h>
-#include <cstdint>
 
-class CanBus {
-private:
-    const struct device* dev_;
+class CanBus
+{
+  private:
+    const struct device *dev_;
     uint32_t bitrate_;
     uint32_t sample_point_;
     bool initialized_;
     bool started_;
 
-public:
+    static void can1_rx_isr(const struct device *dev, struct can_frame *frame, void *user_data);
+    static void can2_rx_isr(const struct device *dev, struct can_frame *frame, void *user_data);
+
+  public:
     CanBus();
-    
-    int init(const struct device* dev, uint32_t bitrate = 1000000, uint32_t sample_point = 875);
+
+    int init(const struct device *dev, uint32_t bitrate = 1000000, uint32_t sample_point = 875);
     int start();
     int stop();
-    
-    int send(const struct can_frame* frame, k_timeout_t timeout, 
-             can_tx_callback_t callback = nullptr, void* user_data = nullptr);
-    
-    int add_rx_filter_msgq(struct k_msgq* msgq, const struct can_filter* filter);
+
+    int send(const struct can_frame *frame, k_timeout_t timeout, can_tx_callback_t callback = nullptr,
+             void *user_data = nullptr);
+
+    int add_rx_filter_msgq(struct k_msgq *msgq, const struct can_filter *filter);
     void remove_rx_filter(int filter_id);
-    
-    int get_state(enum can_state* state) const;
+
+    int get_state(enum can_state *state) const;
     int set_mode(can_mode_t mode);
     can_mode_t get_mode() const;
-    
-    bool is_initialized() const { return initialized_; }
-    bool is_started() const { return started_; }
-    
-    const struct device* get_device() const { return dev_; }
+
+    bool is_initialized() const
+    {
+        return initialized_;
+    }
+    bool is_started() const
+    {
+        return started_;
+    }
+
+    const struct device *get_device() const
+    {
+        return dev_;
+    }
 };
